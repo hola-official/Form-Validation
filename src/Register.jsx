dbfb02a1b1e2c63ac1073ac1725f32bd816f75f8
muhammedola <141998553+muhammedola@users.noreply.github.com>
2023-11-10 10:05:50 +0100

    commit

--- src/Register.jsx
+++ src/Register.jsx
@@ -1,6 +1,10 @@
 import { useEffect, useState, useRef } from "react";
 import { faCheck, faTimes, faInfoCircle } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import axios from './api/axios'
+
+const USER_REGEX = /^[A-Z][A-Z0-9-_]{3,23}$/;
+const PWD_REGEX = /^(?=.*[a-z])(?=.*[a-z])
 
 const Register = () => {
   return (
