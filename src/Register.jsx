6eb40fe3b0b9bbb2dbe239dcfe1b10f9501a807b
muhammedola <141998553+muhammedola@users.noreply.github.com>
2023-11-10 12:06:09 +0100

    commit

--- src/Register.jsx
+++ src/Register.jsx
@@ -65,7 +65,9 @@ const Register = () => {
                 <p id="uidnote" className={userFocus && user && !validName ? 'instruction' :
                     'offscreen'}>
                     <FontAwesomeIcon icon={faInfoCircle} />
-                    3 to 23 characters.<br /> 
+                    3 to 23 characters.<br />  
+                    Must begin with a <br />
+                    letter, number, underscore, hyphens Allowed
 
                 </p>
                 Must include UpperCase and LowerCase Letters, 
