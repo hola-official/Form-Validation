92bfc492d54eb3d04038cc5fa3866e178893a454
muhammedola <141998553+muhammedola@users.noreply.github.com>
2023-11-13 12:12:28 +0100

    commit

--- src/Register.jsx
+++ src/Register.jsx
@@ -64,6 +64,13 @@ const Register = () => {
         <>
             {success ? (
                 <section>
+                    <h1>Registration Completed</h1>
+                    <p>
+                        <a href="#">Sign In</a>
+                    </p>
+                </section>
+            ) : (
+                <section>
                     <p ref={errRef} className={errMsg ? 'errmsg' : 'offscreen'} aria-live="assertive">{errMsg}</p>
                     <h1>Register</h1>
                     <form onSubmit={handleSubmit}>
